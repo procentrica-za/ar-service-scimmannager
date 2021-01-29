ae643f85c3e72f6ac46dae773fa09e751a09a3fe
jmaster98 <jo-ash@procentrica.co.za>
2021-01-29 11:21:28 +0200

    push for gcp

--- src/handlers.go
+++ src/handlers.go
@@ -44,7 +44,7 @@ func (s *Server) verifycredentials() http.HandlerFunc {
 		}
 
 		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-		// TODO: Set InsecureSkipVerify as config in environment.env
+		// TODO: Set InsecureSkipVerify as config in environment.env.
 		client := &http.Client{}
 		data := url.Values{}
 		data.Set("grant_type", "password")
